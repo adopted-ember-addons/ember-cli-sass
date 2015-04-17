3022fa12efa11ee81e0444d810c1cebee180f08f
Pierrick Rouxel <pierrick.rouxel@me.com>
2015-04-17 09:59:32 +0200

    fix config with current broccoli-sass-source-maps version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var checker = require('ember-cli-version-checker');
 var mergeTrees = require('broccoli-merge-trees');
 var merge = require('merge');
+var fs = require('fs');
 
 function SASSPlugin(options) {
   this.name = 'ember-cli-sass';
@@ -22,7 +23,12 @@ SASSPlugin.prototype.toTree = function(tree, inputPath, outputPath, options) {
   if (options.includePaths) trees = trees.concat(options.includePaths);
 
   trees = Object.keys(paths).map(function(file) {
-    var input = path.join(inputPath, file);
+    var input = path.join(inputPath, file + '.scss');
+    // If .scss isn't found, try with .sass
+    if (!fs.existsSync('.' + input)) {
+      input = path.join(inputPath, file + '.sass');
+    }
+
     var output = paths[file];
 
     return new SassCompiler(trees, input, output, options);
