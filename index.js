8ac0f6f46ef28a59d24676b6c60622e86fcb42ae
oozzal <theoozzal@gmail.com>
2016-01-19 13:57:49 +0545

    Compile .sass and .scss out of the box

--- index.js
+++ index.js
@@ -18,12 +18,17 @@ SASSPlugin.prototype.toTree = function(tree, inputPath, outputPath, inputOptions
     inputTrees = inputTrees.concat(options.includePaths);
   }
 
-  var ext = options.extension || 'scss';
+  var exts = options.extensions || ['scss', 'sass'];
   var paths = options.outputPaths;
-  var trees = Object.keys(paths).map(function(file) {
-    var input = path.join(inputPath, file + '.' + ext);
-    var output = paths[file];
-    return new SassCompiler(inputTrees, input, output, options);
+  var trees = [];
+  Object.keys(paths).forEach(function(file) {
+    exts.forEach(function(ext) {
+      var input = path.join(inputPath, file + '.' + ext);
+      if(fs.existsSync('.' + input)) {
+        var output = paths[file];
+        trees.push(new SassCompiler(inputTrees, input, output, options));
+      }
+    });
   });
 
   return mergeTrees(trees);
