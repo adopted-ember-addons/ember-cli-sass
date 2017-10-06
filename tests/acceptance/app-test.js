a90d824995654440f25c84d4a32278aa7670003e
Ryan Rishi <ryan@ryanrishi.com>
2017-10-06 14:12:37 -0700

    use `find` test helper instead of `!!this.$(...)`

--- tests/acceptance/app-test.js
+++ tests/acceptance/app-test.js
@@ -6,28 +6,28 @@ moduleForAcceptance('Acceptance | app');
 test('app.scss is loaded', function(assert) {
   visit('/');
 
-  andThen(function() {
-    assert.ok(!!this.$('#app-css').css('font-family'));
+  andThen(() => {
+    assert.ok(find('#app-css').css('font-family'));
   });
 });
 test('outputPaths are supported', function(assert) {
   visit('/');
 
-  andThen(function() {
-    assert.ok(!!this.$('#output-path-css').css('font-family'));
+  andThen(() => {
+    assert.ok(find('#output-path-css').css('font-family'));
   });
 });
 test('includePaths are supported', function(assert) {
   visit('/');
 
-  andThen(function() {
-    assert.ok(!!this.$('#include-path-css').css('animation-duration'));
+  andThen(() => {
+    assert.ok(find('#include-path-css').css('animation-duration'));
   });
 });
 test('addons can inject styles', function(assert) {
   visit('/');
 
-  andThen(function() {
-    assert.ok(!!this.$('h1').css('font-family'));
+  andThen(() => {
+    assert.ok(find('h1').css('font-family'));
   });
 });
